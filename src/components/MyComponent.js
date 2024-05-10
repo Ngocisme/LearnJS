f41ee04fbcd58b0dc2a01ef318078e6db7fc6a96
Bảo Ngọc <baokhuong6996@gmail.com>
2024-05-10 19:47:56 +0700

    First Project

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render(){
+        return(
+            <>
+                <h1>First Component</h1>
+               <button>Click Con Cac</button>
+            </>
+        )
+    }
+}
+
+export default MyComponent

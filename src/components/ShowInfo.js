a80c419b15bf2cf0a10cb828ffecd61da509325d
Bảo Ngọc <baokhuong6996@gmail.com>
2024-05-13 02:23:07 +0700

    Condition Output and make button show/hide user list

--- src/components/ShowInfo.js
+++ src/components/ShowInfo.js
@@ -2,23 +2,40 @@ import React from "react";
 
 
 class ShowInfor extends React.Component {
+    state = {
+        showListUsers : true
+    }
+    handleShowHide = () => {
+       this.setState({
+        showListUsers : !this.state.showListUsers
+       })
+    }
     render() {
         const { listUsers } = this.props;
-        console.log(listUsers);
         return (
             <>
-                {listUsers.map(
-                    (user) => {
-                        return (
-                            <div key={user.id}>
-                                <br/>
-                                <p>ID {user.id}</p>
-                                <p>My name's {user.name}</p>
-                                <p>My age's {user.age}</p>
-                            </div>
-                        )
-                    }
-                )}
+                <button onClick={() => { this.handleShowHide() }}>{this.state.showListUsers === true ? 'Hide list users' : 'Show list users'}</button>
+                <>
+                    {listUsers.map(
+                        (user) => {
+                            console.log(user)
+
+                            return (
+                                <>
+                                    {this.state.showListUsers &&
+                                        <div key={user.id} className={user.age <= 30 ? "green" : "red"}>
+                                            <br /><p>User have age below 30</p>
+                                            <p>ID {user.id}</p>
+                                            <p>My name's {user.name}</p>
+                                            <p>My age's {user.age}</p>
+                                        </div>
+                                    }
+                                </>
+                            )
+
+                        }
+                    )}
+                </>
             </>
         )
     }
